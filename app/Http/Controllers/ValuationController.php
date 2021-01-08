ee4b3e4a3f681c5abeb9dc6e4cb6b27acb0c3df5
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-08 17:17:43 +0900

    ルート、フォルダ構成作成

---
+++ app/Http/Controllers/ValuationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ValuationController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+}
