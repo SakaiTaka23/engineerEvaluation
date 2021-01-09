7edb1c7ae574863d18f86d27c938ea89309b8976
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-09 16:00:25 +0900

    現時点での関数の型作成

--- app/Service/FetchDataInterface.php
+++ app/Service/FetchDataInterface.php
@@ -4,4 +4,17 @@ namespace App\Service;
 
 interface FetchDataInterface
 {
+    public function fetchPublicRepo();
+    public function fetchComitSum();
+    public function fetchIssues();
+    public function fetchPullRequests();
+    public function fetchStarSum();
+    public function fetchFollowers();
+
+    public function publicRepoFollowers();
+    public function commitStar();
+
+    public function setName(string $name): void;
+
+    public function summarizeData(string $name);
 }
--- app/Service/Production/FetchData.php
+++ app/Service/Production/FetchData.php
@@ -2,8 +2,60 @@
 
 namespace App\Service\Production;
 
+use GuzzleHttp\Client;
 use App\Service\FetchDataInterface;
 
 class FetchData implements FetchDataInterface
 {
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->method = "GET";
+        $this->baseurl = "https://api.github.com/";
+        $this->name = "";
+    }
+
+    public function fetchPublicRepo()
+    {
+    }
+
+    public function fetchComitSum()
+    {
+    }
+
+    public function fetchIssues()
+    {
+    }
+
+    public function fetchPullRequests()
+    {
+    }
+
+    public function fetchStarSum()
+    {
+    }
+
+    public function fetchFollowers()
+    {
+    }
+
+    public function publicRepoFollowers()
+    {
+        $url = $this->baseurl + "users/" + $this->name;
+    }
+
+    public function commitStar()
+    {
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function summarizeData(string $name)
+    {
+        $this->setName($name);
+        // public_repo followers
+    }
 }
