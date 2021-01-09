04c47f5d4e56a8e38532bcb3dfd8d20e2805e234
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-09 10:25:55 +0900

    rank計算実装

--- app/Service/Production/RankData.php
+++ app/Service/Production/RankData.php
@@ -1,7 +1,44 @@
 <?php
 
 namespace App\Service\Production;
+
 use App\Service\RankDataInterface;
+
 class RankData implements RankDataInterface
 {
+    // 上位何%かを表す
+    private const RANK_S_VALUE = 1;
+    private const RANK_DOUBLE_A_VALUE = 25;
+    private const RANK_A2_VALUE = 45;
+    private const RANK_A3_VALUE = 60;
+    // private const RANK_B_VALUE = 100;
+    // TOTAL_VALUES = RANK_S_VALUE + RANK_DOUBLE_A_VALUE + RANK_A2_VALUE + RANK_A3_VALUE + RANK_B_VALUE
+    private const TOTAL_VALUE = 231;
+
+    public function getTotalValue(): int
+    {
+        return RankData::TOTAL_VALUE;
+    }
+
+    public function calcRank(float $normalizedScore): string
+    {
+        $rank = "";
+        switch ($normalizedScore) {
+            case $normalizedScore < RankData::RANK_S_VALUE:
+                $rank = "S+";
+                break;
+            case $normalizedScore >= RankData::RANK_S_VALUE && $normalizedScore < RankData::RANK_DOUBLE_A_VALUE:
+                $rank = "S";
+                break;
+            case $normalizedScore >= RankData::RANK_DOUBLE_A_VALUE && $normalizedScore < RankData::RANK_A2_VALUE:
+                $rank = "A++";
+                break;
+            case $normalizedScore >= RankData::RANK_A2_VALUE && $normalizedScore < RankData::RANK_A3_VALUE:
+                $rank = "A+";
+                break;
+            default:
+                $rank = "B+";
+        }
+        return $rank;
+    }
 }
--- app/Service/RankDataInterface.php
+++ app/Service/RankDataInterface.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Service;
+
 interface RankDataInterface
 {
+    public function calcRank(float $normalizedScore): string;
+
+    public function getTotalValue(): int;
 }
