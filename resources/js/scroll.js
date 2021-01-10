bc32b27a8345e95a5e814e6adbe08e40b0cc04fd
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-10 16:12:31 +0900

    landingページ作成

--- resources/js/scroll.js
+++ resources/js/scroll.js
@@ -1,7 +1,5 @@
 var scrollpos = window.scrollY;
 var header = document.getElementById("header");
-var navcontent = document.getElementById("nav-content");
-var navaction = document.getElementById("navAction");
 var brandname = document.getElementById("brandname");
 var toToggle = document.querySelectorAll(".toggleColour");
 
@@ -11,24 +9,14 @@ document.addEventListener("scroll", function () {
 
     if (scrollpos > 10) {
         header.classList.add("bg-white");
-        navaction.classList.remove("bg-white");
-        navaction.classList.add("gradient");
-        navaction.classList.remove("text-gray-800");
-        navaction.classList.add("text-white");
         //Use to switch toggleColour colours
         for (var i = 0; i < toToggle.length; i++) {
             toToggle[i].classList.add("text-gray-800");
             toToggle[i].classList.remove("text-white");
         }
         header.classList.add("shadow");
-        navcontent.classList.remove("bg-gray-100");
-        navcontent.classList.add("bg-white");
     } else {
         header.classList.remove("bg-white");
-        navaction.classList.remove("gradient");
-        navaction.classList.add("bg-white");
-        navaction.classList.remove("text-white");
-        navaction.classList.add("text-gray-800");
         //Use to switch toggleColour colours
         for (var i = 0; i < toToggle.length; i++) {
             toToggle[i].classList.add("text-white");
@@ -36,7 +24,5 @@ document.addEventListener("scroll", function () {
         }
 
         header.classList.remove("shadow");
-        navcontent.classList.remove("bg-white");
-        navcontent.classList.add("bg-gray-100");
     }
 });
