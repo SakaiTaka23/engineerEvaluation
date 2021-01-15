d824cd53bdac824f8ecb3dceb51d602c8bb4170f
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-15 11:43:50 +0900

    viewが動的な値に対応

--- app/Http/Controllers/Models/ResultViewModel.php
+++ app/Http/Controllers/Models/ResultViewModel.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Models;
 
 class ResultViewModel
 {
-    public function __construct(int $publicRepo, int $commitSum, int $issues, int $pullRequest, int $starSum, int $followers)
+    public function __construct(string $userName, int $publicRepo, int $commitSum, int $issues, int $pullRequest, int $starSum, int $followers, string $rank)
     {
+        $this->userName = $userName;
         $this->publicRepo = $publicRepo;
         $this->commitSum = $commitSum;
         $this->issues = $issues;
         $this->pullRequest = $pullRequest;
         $this->starSum = $starSum;
         $this->followers = $followers;
+        $this->rank = $rank;
     }
 }
--- app/Http/Controllers/ValuationController.php
+++ app/Http/Controllers/ValuationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Models\ResultViewModel;
 use App\Rules\UserExists;
 use Illuminate\Http\Request;
 
@@ -20,13 +21,13 @@ class ValuationController extends Controller
             'userName' => ['required', new UserExists],
         ]);
         $username = $request->userName;
-        dd($username);
         /**
          * @todo 受け取った後に名前が有効かどうか確認すること!!!
          */
         // ↓ビュー確認後コメントアウト
         // $rank = $this->rank->evaluation("SakaiTaka23");
-        $rank = ["A+"];
-        return view('result', 'rank');
+        // 擬似データ
+        $viewModel = new ResultViewModel("SakaiTaka23", 1, 1, 1, 1, 1, 1, "A+");
+        return view('result', compact('viewModel'));
     }
 }
