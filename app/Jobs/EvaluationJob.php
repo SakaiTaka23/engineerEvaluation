e2e80640fe174892db877063944f5eb59fa812a8
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-23 17:04:17 +0900

    判定後メールが送られるように調整

--- app/Jobs/EvaluationJob.php
+++ app/Jobs/EvaluationJob.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -13,6 +12,8 @@ use Illuminate\Support\Facades\Artisan;
 class EvaluationJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $id;
 
     /**
      * Create a new job instance.
@@ -31,7 +32,7 @@ class EvaluationJob implements ShouldQueue
      */
     public function handle()
     {
-        Artisan::command('task:evaluate '.$this->id);
-        Artisan::command('task:sendresult ' . $this->id);
+        Artisan::call('task:evaluate ' . $this->id);
+        Artisan::call('task:sendresult ' . $this->id);
     }
 }
